436a1c98a66f6ca86ae129af364001d6082a6d7d
Qiushi Yan <qiushi.yann@gmail.com>
2023-05-30 14:48:28 +0800

    update first module

--- apps/example-poe/src/components/toc-sidebar.tsx
+++ apps/example-poe/src/components/toc-sidebar.tsx
@@ -30,8 +30,7 @@ export function TocSidebar({ headings }: TocSidebarProps) {
 								className={cn("hover:underline inline-flex ", {
 									"text-base": heading.level === "one",
 									"text-sm": heading.level === "two",
-									"text-muted-foreground text-xs pl-2":
-										heading.level === "three",
+									"text-primary/70 text-xs pl-2": heading.level === "three",
 								})}
 							>
 								{heading.text}
