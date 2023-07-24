dda4fc96d889bc79194b1d55df6f64e016a63e92
Qiushi Yan <qiushi.yann@gmail.com>
2023-07-24 19:14:05 +0800

    add files

---
+++ apps/example-think-python/src/components/mdx-components.tsx
@@ -0,0 +1,32 @@
+import {
+	Info,
+	Warning,
+	Keyterm,
+	Callout,
+	Caption,
+	Typography,
+	Blockquote,
+	Definition,
+} from "@itell/ui/server";
+import { Exercise } from "./exercise";
+import { CodeExample } from "@/components/exercise/code-example";
+import { YoutubeVideo } from "@/components/ui/youtube";
+import { Image } from "./ui/image";
+
+
+export const MdxComponents = {
+	// YoutubeVideo,
+	Blockquote,
+	Info,
+	Warning,
+	Keyterm,
+	Callout,
+	Caption,
+	Definition,
+	Typography,
+	// exercise related
+	Exercise,
+	CodeExample,
+	YoutubeVideo,
+	Image,
+};
