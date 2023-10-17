2c3b22d95cb2aa487f0b50a002fb6dc712499e1d
Qiushi Yan <qiushi.yann@gmail.com>
2023-10-17 19:46:57 +0800

    fix build error

--- apps/example-think-python/src/app/api/note/route.ts
+++ apps/example-think-python/src/app/api/note/route.ts
@@ -2,9 +2,11 @@ import db from "@/lib/db";
 
 export async function POST(request: Request) {
 	const data = (await request.json()) as { id: string };
-	return await db.note.delete({
+	await db.note.delete({
 		where: {
 			id: data.id,
 		},
 	});
+
+	return;
 }
