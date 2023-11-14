7c69bf8540b65d903097f16cb55869702593712b
Qiushi Yan <qiushi.yann@gmail.com>
2023-11-14 15:20:56 -0600

    add loading indicator to cotinue-reading-button

--- apps/example-think-python/src/components/start-or-continue-reading.tsx
+++ apps/example-think-python/src/components/start-or-continue-reading.tsx
@@ -3,15 +3,29 @@
 import { useLastVisitedChapterUrl } from "@/lib/hooks/use-last-visited-chapter";
 import Link from "next/link";
 import { buttonVariants } from "@itell/ui/server";
+import { useTransition } from "react";
+import Spinner from "./spinner";
+import { Button } from "./client-components";
+import { useRouter } from "next/navigation";
 
 export const StartOrContinueReading = () => {
 	const url = useLastVisitedChapterUrl();
+	const router = useRouter();
 	const text = url ? "Continue Reading" : "Start Reading";
 	const href = url || "/chapter-0";
+	const [pending, startTransition] = useTransition();
 
 	return (
-		<Link href={href} className={buttonVariants({ size: "lg" })}>
-			{text}
-		</Link>
+		<Button
+			onClick={() => {
+				startTransition(() => {
+					router.push(href);
+				});
+			}}
+			disabled={pending}
+			size="lg"
+		>
+			{pending && <Spinner className="w-4 h-4 mr-2" />} {text}
+		</Button>
 	);
 };
